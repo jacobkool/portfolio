839bb48c803733588a64f165a0c2271a82427955
Jake Kool <jacob.kool@procore.com>
2022-04-11 16:26:34 -0400

    adding projects component

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './app.css'
 import AboutMe from './components/AboutMe'
 import Learning from './components/Learning'
+import Projects from './components/Projects'
 import { useRef } from 'react';
 
 const App: React.FC = () => {
@@ -25,6 +26,27 @@ const App: React.FC = () => {
     }
     
     }
+
+    const projectsData = [
+        {
+            screenshot: 'https://content.screencast.com/users/jacob.kool/folders/Snagit/media/9c362ffa-d15f-4a01-80bf-47da6c642e2f/04.05.2022-09.55.png',
+            name: 'Nba Stats Project',
+            Vercel: 'https://nbastats-delta.vercel.app/#/',
+            Github: 'https://github.com/jacobkool/NBA-Scores-Players-Teams'
+        },
+         {
+            screenshot: 'https://content.screencast.com/users/jacob.kool/folders/Snagit/media/934502e5-a0ec-42ac-b03b-550cd4b3641e/04.05.2022-09.56.png',
+            name: 'React-Redux-Store',
+            Vercel: 'https://react-redux-shopping-cart-jacobkool.vercel.app/',
+            Github: 'https://github.com/jacobkool/React-Redux-Shopping-Cart'
+        },
+        {
+            screenshot: 'https://content.screencast.com/users/jacob.kool/folders/Snagit/media/96e428ea-8796-4962-88a2-e9a45f34d2b4/04.05.2022-09.54.png',
+            name: 'React-Redux-Store',
+            Vercel: 'https://react-todo-app-olive.vercel.app/',
+            Github: 'https://github.com/jacobkool/react-todo-app'
+        }
+    ];
    
     return (
         <div className='app_container'>
@@ -44,8 +66,8 @@ const App: React.FC = () => {
                 <div className="frameworks" ref={frameworksRef}>
                     <Learning object={{name: 'Jake', favNumber: 12}}/>
                 </div>
-                <div className="learning" ref={portfolioRef}>
-                    Projects
+                <div className="projects" ref={portfolioRef}>
+                    <Projects projects={projectsData}/>
                 </div>
                 <div className="footer">
                     THIS IS THE FOOTER
